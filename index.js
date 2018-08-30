f51311f8ea38e9cbb43d1f1fed5967992687ac31
Anderson <s0044530@lstllandersonmac.rgare.net>
2018-08-30 09:24:19 -0500

    echo more things

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ app.use(bodyParser.text({
   type: req => 'text'
 }));
 
-app.post('/post', (req, res) => {
+app.use((req, res) => {
   res.status(200).send({
+    path: req.path,
+    method: req.method,
+    query: req.query,
     headers: req.headers,
+    cookies: req.cookies,
     body: req.body
   })
 })
